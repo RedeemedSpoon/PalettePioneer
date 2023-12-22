2234c638f5a9ed848fbf6ee89e3945d4dd7813f1
Ethan <QustoTemmo@protonmail.com>
2023-12-22 15:24:20 +0100

    Holala!

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, abort, make_response
 from PIL import Image, UnidentifiedImageError
 from datetime import datetime
 import numpy as np
 import io
 
 app = Flask(__name__)
-app.secret_key = '5db46df5b1b5n15gs1f56sg1t5665n1f6s2dg5er' # Very insecure and improfessional but idc
+
 @app.route('/')
 def index():
       date = datetime.now().year
@@ -13,16 +13,25 @@ def index():
 
 @app.route('/results', methods=['GET', 'POST'])
 def scan():
-      if request.method == 'POST':
-            try:
-                  f = request.files['imageinput']
-                  img = Image.open(io.BytesIO(f.read()))
-                  img_array = np.array(img)
-                  
-                  return "Image received and converted to np.array successfully!"
-            except UnidentifiedImageError:
-                  flash('Error: Please upload a valid image file.')
-                  return redirect('/')
-
+    if request.method == 'POST':
+        try:
+            file = request.files['imageinput']
+            filename = file.filename
+            img = Image.open(io.BytesIO(file.read()))
+            image = np.array(img)
+            
+            flattened_img_array = image.reshape(-1, 3)
+            unique_colors, counts = np.unique(flattened_img_array, axis=0, return_counts=True)
+            sorted_indices = np.argsort(counts)[::-1]
+            sorted_colors = unique_colors[sorted_indices]
+            top_colors = sorted_colors[:10]
+            
+            response = make_response(render_template("results.html", colors=top_colors))
+            response.headers['Content-Type'] = 'text/html'
+            return response
+      
+        except UnidentifiedImageError:
+            return abort(415)
+        
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- static/script.js
+++ static/script.js
@@ -1,15 +1,57 @@
 document.addEventListener("DOMContentLoaded", function () {
+   const imageInput = document.getElementById("imageinput");
+   const importButton = document.getElementById("importbutton");
+   const loadingModal = document.getElementById("loadingmodal");
+   const dotsElement = document.getElementById("dots");
+
    document.getElementById("imagecontainer").addEventListener("click", function () {
-      document.getElementById("imageinput").click();
+      imageInput.click();
    });
 
-   document.getElementById("importbutton").addEventListener("click", function () {
-      document.getElementById("imageinput").click();
+   importButton.addEventListener("click", function () {
+      imageInput.click();
    });
 
-   document.getElementById("imageinput").addEventListener("change", function () {
+   imageInput.addEventListener("change", function () {
       if (this.files?.[0]) {
-         document.getElementById("uploadForm").submit();
+         showLoadingModal();
+
+         const formData = new FormData();
+         formData.append("imageinput", this.files[0]);
+
+         fetch("/results", {
+            method: "POST",
+            body: formData,
+         })
+            .then((response) => response.text())
+            .then((html) => {
+               document.body.innerHTML = html;
+               hideLoadingModal();
+            });
       }
    });
+
+   function showLoadingModal() {
+      loadingModal.style.display = "block";
+      startLoadingAnimation();
+   }
+
+   function hideLoadingModal() {
+      loadingModal.style.display = "none";
+   }
+
+   function startLoadingAnimation() {
+      let counter = 1;
+
+      function animateDots() {
+         dotsElement.textContent += ".";
+         counter++;
+         if (counter > 5) {
+            counter = 1;
+            dotsElement.textContent = "";
+         }
+         setTimeout(animateDots, 750);
+      }
+      animateDots();
+   }
 });
